fff17f3f4238f42a9fc3729569d4a0fe68adfa8d
Heather Drattlo <hdrattlo88@yahoo.com>
2019-12-01 21:36:21 -0700

    added packages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/fww/index.html'));
 });
 
 // Start the app by listening on the default
