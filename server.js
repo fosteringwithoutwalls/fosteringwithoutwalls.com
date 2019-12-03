8cb781b16f918ae53751e091ac50d4af50d57ecf
Heather Drattlo <hdrattlo88@yahoo.com>
2019-12-03 07:48:30 -0700

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 // const html = __dirname + 'src';
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json());
+app.use(bodyParser());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
